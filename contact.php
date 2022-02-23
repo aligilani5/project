8fda315e81837789884ab35fd992ab368edafeee
ali2-wpbrigade <ali3.wpbrigade@gmail.com>
2022-02-23 08:15:58 -0800

    space

--- contact.php
+++ contact.php
@@ -15,8 +15,8 @@
     <h4 class="card-title txt">Reach Us Through <i class = "fa fa-arrow-down"></i></h4>
     <br>
     <br>
-    <p style = "font-size: 18px;" class = "txt"><i class = "fa fa-whatsapp" aria-hidden = "true"></i>: <a href="https://wa.me/92349739526" target="_blank" rel="noopener noreferrer"> +92 3249739526 </a></p>
-    <p style = "font-size: 18px;" class = "txt"><i class = "fa fa-envelope" aria-hidden = "true"></i>: <a href="mailto:agilani482@gmail.com" target="_blank" rel="noopener noreferrer"> contact@instruments.com</a></p>
+    <p style = "font-size: 18px;" class = "txt"><i class = "fa fa-whatsapp" aria-hidden = "true"></i> : <a href="https://wa.me/92349739526" target="_blank" rel="noopener noreferrer"> +92 3249739526 </a></p>
+    <p style = "font-size: 18px;" class = "txt"><i class = "fa fa-envelope" aria-hidden = "true"></i> : <a href="mailto:agilani482@gmail.com" target="_blank" rel="noopener noreferrer"> contact@instruments.com</a></p>
 </form>
 
 <?php require 'includes/footer.php'; ?>
