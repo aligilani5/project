6ca364272661a3a5b73185dfbbe6679769a269d8
ali2-wpbrigade <ali3.wpbrigade@gmail.com>
2022-02-25 07:01:11 -0800

    icon

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Instruments</title>
+   <link rel = "icon" href = "css/profile.jpg"/>
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
    <link rel="stylesheet" type="text/css" href="css/style.css" />
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
